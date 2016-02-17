a770ba91d9b6c916c522b235de51d726eb1cbb83
kavitha_budumuri <venkatakavitha.budumuri@gmail.com>
2016-02-17 13:39:20 -0700

    mongo model

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
-var app=require('express')();
-var http=require('http').Server(app);
+var app = require('express')();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var path=require("path");
+var path = require("path");
+var mongoose = require('mongoose');
+var Message = require('./models/messagemodel');
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+mongoose.connect('mongodb://localhost:27017/TOLC-chat');
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-var usernames={};
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('adduser',function(username){
-  	socket.username=username;
-        usernames[username]=username;
-        io.sockets.emit('enteruser',username);
-  
-});
-  socket.on('sendchat',function(data){
-        
-  	io.sockets.emit('updatechat',socket.username,data);
-});
-  socket.on('disconnect',function(){
- 	username=socket.username;	
-	io.sockets.emit('deleteuser',username);
-});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/chat',function(req,res){
-  res.sendFile(path.join(__dirname+'/chat.html'));
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
 
+var usernames = {};
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        usernames[username] = username;
+        io.sockets.emit('enteruser', username);
+
+        Message.find(function(err,data){
+          if(err) console.log(err);
+          else{
+            data.forEach(function(mes){
+              io.sockets.emit('updatechat',mes.user,mes.text);
+            });
+          }
+        })
+
+    });
+    socket.on('sendchat', function(data) {
+        var message = new Message({
+          user: socket.username,
+          text: data
+        });
+        message.save(function(err){
+          if(err) console.log(err);
+          else console.log("saved");
+        });
+        io.sockets.emit('updatechat', socket.username, data);
+    });
+    socket.on('disconnect', function() {
+        username = socket.username;
+        io.sockets.emit('deleteuser', username);
+    });
+});
+
+app.get('/chat', function(req, res) {
+    res.sendFile(path.join(__dirname + '/chat.html'));
+});
