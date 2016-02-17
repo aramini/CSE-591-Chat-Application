dec6a368fb6bda6d8960d3b84c6002b3af3b4386
Archana <archana@archana-Inspiron-5547.Home>
2016-02-16 20:07:25 -0700

    modified chat and app js files

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3001, function(){
+  console.log('listening on *:3001');
 });
 
 var usernames={};
